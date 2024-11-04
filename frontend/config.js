b569e8066cbaa08fc9a9a30ece1e8edc47021a36
dsrouka <dsrouka@nolatec.org>
2024-11-04 13:49:27 +0000

    Paramétrage  des urls du frontend  autres modifications

---
+++ frontend/config.js
@@ -0,0 +1,10 @@
+// config.js
+const config = {
+    ni_url: "http://127.0.0.1:8002",
+    atm_url: "http://127.0.0.1:8001",
+    core_bankin_url: "http://127.0.0.1:8000"
+  };
+  
+  // Exporter le module pour l'importation
+  export default config;
+  
