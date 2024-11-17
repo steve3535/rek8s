513344f56664452130175f70164b92e9c3fe699d
dsrouka <dsrouka@nolatec.org>
2024-11-17 19:41:01 +0000

    tous les services  conteneuriser

--- frontend/config.js
+++ frontend/config.js
@@ -1,10 +1,8 @@
-// config.js
 const config = {
-    ni_url: "http://127.0.0.1:8002",
-    atm_url: "http://127.0.0.1:8001",
-    core_banking_url: "http://127.0.0.1:8000"
-  };
-  
-  // Exporter le module pour l'importation
-  export default config;
-  
+  ni_url: "http://ni_switch_service:8002",
+  atm_url: "http://atm_service:8001",
+  core_banking_url: "http://core_banking_service:8000"
+};
+
+// Exporter le module pour l'importation
+export default config;
